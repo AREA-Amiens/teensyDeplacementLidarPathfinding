fb7ff9a85e70c73e8b34899f2b07c6c11d6034b5
kingovchouffe <46623009+kingovchouffe@users.noreply.github.com>
2020-01-19 00:02:58 +0100

    version rplidar probleme detection

--- src/deplacement.h
+++ src/deplacement.h
@@ -15,22 +15,23 @@ typedef struct{ //struture pour enregistrer les x et y en même temps
 }pos;
 
 #define my_adr 10         //mon adresse bus I2C
-#define reset_G   15 // pin du moteur gauche
-#define sleep_G   13
-#define step_G    0
-#define dir_G     1
+#define reset_G   23 // pin du moteur gauche
+#define sleep_G   22
+#define step_G    21
+#define dir_G     20
 
-#define reset_D   9// pin du moteur droit
-#define sleep_D   10
-#define step_D    11
-#define dir_D     12
+#define reset_D   3// pin du moteur droit
+#define sleep_D   4
+#define step_D    5
+#define dir_D     6
 
 
-#define acceleration  5200//accélération du robot x 2coéficient_go
-#define speed         250
+
+#define acceleration  5000//accélération du robot x 2coéficient_go
+#define speed         1000
 
 #define coeficien_turn /*12.99399105*/  -4.081256839 //coefficiant determiné par rapport au diamètre de roue, sélection du pas(ici 1/4),le tout pour 1°
-#define coeficien_go   -19.32552 //coefficiant determiné par rapport au diamètre de roue, sélection du pas(ici 1/4),le tout pour 1mm
+#define coeficien_go  -4.65705559 //coefficiant determiné par rapport au diamètre de roue, sélection du pas(ici 1/4),le tout pour 1mm
 
 #define pi 3.1415926536//
 
@@ -50,7 +51,7 @@ byte iso_bite(byte analiser, byte decalage);
 //dealage = 2
 //return = 0000 00x00
 
-pos position ();//decode les trames envoyer par byte par la carte stratégie pour les x et y
+void position ();
 //renvoie position de x et y
 void recalageY (pos &pos1);//recalage en Y en fonction du mur, et de l'av ou ar
 void recalageX (pos &pos1);//recalage en X en fonction du mur, et de l'av ou ar
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <pathfindingteensy32.h>
 #include <deplacement.h>
-#include <YDLidar.h>
+#include <rplidar.h>
 
 AccelStepper motor_G(1, step_G, dir_G);//declaration du moteur gauche
 AccelStepper motor_D(1, step_D, dir_D);//declatation du moteur droit
 
-YDLidar lidar;
-
-#define YDLIDAR_MOTOR_SCTP 3 // The PWM pin for control the speed of YDLIDAR's motor.
-#define YDLIDAR_MOTRO_EN   7 // The ENABLE PIN for YDLIDAR's motor
+RPLidar lidar;
+rplidar_response_device_info_t info;
+#define RPLIDAR_MOTOR 12 // The PWM pin for control the speed of YDLIDAR's motor.
 
 int xp=100,yp=0,ap=0;
 int go=0, turndepart=0,turndepartp=0,turnarrive=0,turnarrivep=0,turnar,turnarp,turnactu=0,cons=0;
-noeud positionCourante;
+int r;
+int alpha;
+
+noeud positionrobot;
 noeud pospre;
 noeud posdeb;
 noeud listeCheminNouveau[150];
@@ -25,72 +27,28 @@ bool isScanning=false;
 long pas;//varible coréspondant a la distance a par courir elle ne comprote pas de sance
 int sense;//sense variable relative au sance de la rotation ou de la direction du go
 
-noeud position(noeud pospre){
-  int r = motor_D.currentPosition()/coeficien_go+motor_G.currentPosition()/coeficien_go;
-  int alpha = (motor_D.currentPosition()/coeficien_go-motor_G.currentPosition()/coeficien_go)*coeficien_turn;
-  noeud posrobot;
-  posrobot.x = pospre.x + r + cos(alpha);
-  posrobot.y = pospre.y + r + sin(alpha);
-  motor_D.setCurrentPosition(0);
-  motor_G.setCurrentPosition(0);
-  motor_D.setSpeed(100);
-  motor_G.setSpeed(100);
-  return posrobot;
-}
-
-void restartScan(){
-    device_info deviceinfo;
-    if (lidar.getDeviceInfo(deviceinfo, 100) == RESULT_OK) {
-         int _samp_rate=4;
-         String model;
-         float freq = 7.0f;
-         switch(deviceinfo.model){
-            case 1:
-                model="F4";
-                _samp_rate=4;
-                freq = 7.0;
-                break;
-            case 4:
-                model="S4";
-                _samp_rate=4;
-                freq = 7.0;
-                break;
-            case 5:
-                model="G4";
-                _samp_rate=9;
-                freq = 7.0;
-                break;
-            case 6:
-                model="X4";
-                _samp_rate=5;
-                freq = 7.0;
-                break;
-            default:
-                model = "Unknown";
-          }
-
-          uint16_t maxv = (uint16_t)(deviceinfo.firmware_version>>8);
-          uint16_t midv = (uint16_t)(deviceinfo.firmware_version&0xff)/10;
-          uint16_t minv = (uint16_t)(deviceinfo.firmware_version&0xff)%10;
-          if(midv==0){
-            midv = minv;
-            minv = 0;
-          }
-          delay(100);
-          device_health healthinfo;
-          if (lidar.getHealth(healthinfo, 100) == RESULT_OK){
-
-              if(lidar.startScan() == RESULT_OK){
-                isScanning = true;
-
-
-
-}}}}
+// void position(){
+//   int r = motor_D.currentPosition()/coeficien_go+motor_G.currentPosition()/coeficien_go;
+//   int alpha = (motor_D.currentPosition()/coeficien_go-motor_G.currentPosition()/coeficien_go)*coeficien_turn;
+//   positionrobot.x = pospre.x + r + cos(alpha);
+//   positionrobot.y = pospre.y + r + sin(alpha);
+//   motor_D.setCurrentPosition(0);
+//   motor_G.setCurrentPosition(0);
+//   motor_D.setSpeed(speed);
+//   motor_G.setSpeed(speed);
+// }
 
 //le setup ne sexcute qune seul foi lor du demarage de la teensi
 void setup(){
-
-  lidar.begin(Serial2, 128000);
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13,HIGH);
+  Serial2.begin(115200);
+  lidar.begin(Serial2);
+  pinMode(RPLIDAR_MOTOR, OUTPUT);
+  lidar.getDeviceInfo(info, 100);
+  digitalWrite(RPLIDAR_MOTOR,HIGH);
+  lidar.startScan();
   initTable();
   //initialisation du moteur gauche
   pinMode(reset_G, OUTPUT);    //le resete se fait a l'aita bas
@@ -98,7 +56,7 @@ void setup(){
   pinMode(sleep_G, OUTPUT);    //le sleep se met a l'aita bas poru une carte fonctionelle
   digitalWrite(sleep_G, HIGH);
 
-  //motor_G.setSpeed(speed);
+  motor_G.setSpeed(speed);
   motor_G.setAcceleration(acceleration);
 
   //initialisation du moteur doit
@@ -107,26 +65,40 @@ void setup(){
   pinMode(sleep_D, OUTPUT);    //le sleep se met a l'aita bas poru une carte fonctionelle
   digitalWrite(sleep_D, HIGH);
 
-  //motor_D.setSpeed(speed);
+  motor_D.setSpeed(speed);
   motor_D.setAcceleration(acceleration);
-
+  // MsTimer2::set(500, position);
+  // MsTimer2::start();
   posdeb.x=50;
   posdeb.y=50;
   table2020[50][50]=2;
   table2020[100][50]=3;
   pospre=posdeb;
   arrive.x=100;
-  arrive.y=100;
+  arrive.y=50;
 
 
 }
-void loop() {
-  positionCourante = position(pospre);
-  if(isScanning){
-    if (lidar.waitScanDot() == RESULT_OK) {
 
-      float distance = lidar.getCurrentScanPoint().distance; //distance value in mm unit
-      float angle    = lidar.getCurrentScanPoint().angle; //anglue value in degrees
+void loop() {
+ r = motor_D.currentPosition()/coeficien_go+motor_G.currentPosition()/coeficien_go;
+ alpha = (motor_D.currentPosition()/coeficien_go-motor_G.currentPosition()/coeficien_go)*coeficien_turn;
+
+ positionrobot.x = pospre.x + r + cos(alpha);
+ positionrobot.y = pospre.y + r + sin(alpha);
+
+
+  pospre.x=r+cos(alpha);
+  pospre.y=r+sin(alpha);
+  go = (int)sqrt((double)pow((arrive.x*20-posdeb.x*20),2)+pow((arrive.y*20-posdeb.y*20),2));
+  pas=(long)(coeficien_go*(float)(go));
+  motor_D.move(pas);//activation de la rotation jusque cette valeur de pas moteur droite
+  motor_G.move(-pas);
+
+if (IS_OK(lidar.waitPoint())) {
+  if(motor_D.isRunning()){
+      float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
+      float angle  = lidar.getCurrentPoint().angle; //anglue value in degrees
 
       if(((360-15)<angle||angle<=15)&&distance<300&&distance!=0){//détection
 
@@ -136,19 +108,19 @@ void loop() {
         int posxenemi= distance*cos(angle);
         int posyenemi= distance*sin(angle);
         posEnemi(posxenemi,posyenemi);
-        nbrnoeud1=algoPAstar(table2020, arrive, positionCourante);
+        nbrnoeud1=algoPAstar(table2020, arrive, positionrobot);
         listeCheminNouveau[nbrnoeud1] = cheminRobot();
         for(int i =0;i<nbrnoeud1;i++){
 
-          go = (int)sqrt((double)pow((listeCheminNouveau[i].x-positionCourante.x),2)+pow((listeCheminNouveau[i].y-positionCourante.y),2));
+          go = (int)sqrt((double)pow((listeCheminNouveau[i].x-positionrobot.x),2)+pow((listeCheminNouveau[i].y-positionrobot.y),2));
 
-          if(positionCourante.x!=listeCheminNouveau[i].x){
-            turndepart=(int)((float)atan((double)(((float)(listeCheminNouveau[i].y-positionCourante.y))/((float)(listeCheminNouveau[i].x-positionCourante.x))))*(float)180/pi);
-            if(positionCourante.x>listeCheminNouveau[i].x)turndepart+=360;
+          if(positionrobot.x!=listeCheminNouveau[i].x){
+            turndepart=(int)((float)atan((double)(((float)(listeCheminNouveau[i].y-positionrobot.y))/((float)(listeCheminNouveau[i].x-positionrobot.x))))*(float)180/pi);
+            if(positionrobot.x>listeCheminNouveau[i].x)turndepart+=360;
             else turndepart+=180;
             turndepart%=360;
           }else{
-            if(positionCourante.y>listeCheminNouveau[i].y)turndepart=90;
+            if(positionrobot.y>listeCheminNouveau[i].y)turndepart=90;
             else turndepart=270;
                 }
 
@@ -167,16 +139,17 @@ void loop() {
                   motor_G.setCurrentPosition(0);
                   motor_D.move(pas);//activation de la rotation jusque cette valeur de pas moteur droite
                   motor_G.move(-pas);//activation de la rotation jusque cette valeur de pas moteur gauche
+                  motor_D.run();//lancemant du moteur droit
+                  motor_G.run();//lancemant du moteur gauche
                 }
         }
 
       }
-    }else{
-       //Serial.println(" YDLIDAR get Scandata failed!!");
     }
-  }else{
-      //stop motor
-    restartScan();
+
   }
+
+  motor_D.run();//lancemant du moteur droit
+  motor_G.run();//lancemant du moteur gauche
 
 }
